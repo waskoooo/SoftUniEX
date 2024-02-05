39439a895102417c71908414775cd2a39734a46f
waskoooo <vasiltarashev@gmail.com>
2024-02-05 17:42:20 +0200

    project creation

---
+++ Basics/ex_7.py
@@ -0,0 +1,6 @@
+architect_name = input()
+project_count = int(input())
+
+hours_needed = project_count * 3
+
+print(f"The architect {architect_name} will need {hours_needed} hours to complete {project_count} project/s.")
