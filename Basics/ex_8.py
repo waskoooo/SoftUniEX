8a7b435d85c0f40bb8826442477b0bf173bd6e6d
waskoooo <vasiltarashev@gmail.com>
2024-02-05 17:45:41 +0200

    pet shop

---
+++ Basics/ex_8.py
@@ -0,0 +1,8 @@
+dog_food_count = int(input())
+cat_food_count = int(input())
+
+dog_food = 2.50
+cat_food = 4
+
+final_cost = dog_food_count * dog_food + cat_food * cat_food_count
+print(f"{final_cost} lv.")
